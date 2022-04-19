d8421ebbab5d2e5e0c27037cf87c7e3236177366
Jonty <jmarshall@asiaa.sinica.edu.tw>
2022-04-19 22:49:01 +0800

    Corrected typo
    Wavelength arrays now correctly defined in the extrapolation function.

--- distillery.py
+++ distillery.py
@@ -234,7 +234,7 @@ def Extrapolation(species,wave_min=0.1,wave_max=1000.0,nlow=100,nhigh=100,logspa
       if logspace == True :
         extra_wave_lo = np.logspace(np.log10(0.9*wave_min),np.log10(wave[0]),num=nlow,base=10.0,endpoint=True)
       else: 
-        extra_wave = np.linspace(0.9*wave_min,wave[0],num=nlow)
+        extra_wave_lo = np.linspace(0.9*wave_min,wave[0],num=nlow)
 
       slope_real_lo = (real[1] - real[0]) / (wave[1] - wave[0])
       extra_real_lo = real[0] + (slope_real_lo * (extra_wave_lo - wave[0]))
@@ -246,7 +246,7 @@ def Extrapolation(species,wave_min=0.1,wave_max=1000.0,nlow=100,nhigh=100,logspa
       if logspace == True :
         extra_wave_hi = np.logspace(np.log10(wave[-1]),np.log10(1.1*wave_max),num=100,base=10.0,endpoint=True)
       else: 
-        extra_wave = np.linspace(wave[-1],1.1*wave_max,num=nhigh)
+        extra_wave_hi = np.linspace(wave[-1],1.1*wave_max,num=nhigh)
 
       slope_real_hi = (real[-2] - real[-1]) / (wave[-2] - wave[-1])
       extra_real_hi = real[-1] + (slope_real_hi * (extra_wave_hi - wave[-1]))
